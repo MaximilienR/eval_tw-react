502c2814e14294002da8d9b425b0e590af052473
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2025-04-03 14:10:19 +0200

    add comment to code

--- src/coponents/Clients.jsx
+++ src/coponents/Clients.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import OneClient from './OneClient';
 
 export default function Clients() {
+  /*{ tableau avis  des clients}}*/
     const reviews = [
         {
           id: 1,
@@ -13,11 +14,13 @@ export default function Clients() {
           name: "Thomas B.",
           review: "Hôte très sympathique, je recommande à 100% !",
         },
-        { id: 3, name: "Emma D.", review: "Excellente expérience, on reviendra." },
+        { id: 3,
+          name: "Emma D.", 
+          review: "Excellente expérience, on reviendra." },
       ];
     return (<>
 <h6 class="text-lg font-bold dark:text-white  ml-5 ">AVIS CLIENT</h6>
-{/* conteneur de grille avec 3 colonnes  */}
+/*{ conteneur de grille avec 3 colonnes  }*/
 <div class="grid    md:grid-cols-3 ">
 
 
--- src/coponents/OneClient.jsx
+++ src/coponents/OneClient.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function OneClient({client}) {
   return (
-    /*{rouded pour arrondir les bords, donner une taille fix pour aux images pour plus qui s'écarte }*/
+    /*{rouded pour arrondir les bords,Donner une taille fixe aux images pour éviter qu'elles ne se déforment. }*/
     <div class="max-w-xs p-4 bg-white border border-gray-200 rounded-md shadow-sm dark:bg-gray-800 dark:border-gray-700 ml-3 hover:scale-110 transition duration-300">
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{client.name}</p>
       <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{client.review}</p>
